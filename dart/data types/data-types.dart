857afd4c5eff56c9333a670aa2ee65e58eabec0a
Nathan-cloud-dev <kiprotichnathan@yahoo.com>
2024-03-07 11:04:44 +0300

    data types

---
+++ dart/data types/data-types.dart
@@ -0,0 +1,22 @@
+// Main function to demonstrate data types in Dart
+void main() {
+  // Integer data type
+  int age = 25;
+  print('Integer: $age');
+
+  // Double data type
+  double height = 1.75;
+  print('Double: $height');
+
+  // String data type
+  String name = 'Kiprotich Nathan';
+  print('String: $name');
+
+  // List data type
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('List: $numbers');
+
+  // Map data type
+  Map<String, int> ages = {'John': 25, 'Jane': 30};
+  print('Map: $ages');
+}
